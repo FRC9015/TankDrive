a29e434f565474fa5df2cfa44786c591e92c5aec
levi robison <levisrobison@gmail.com>
2023-11-22 17:31:39 -0500

    Fixed syntax errors

--- src/main/java/frc/robot/subsystems/LimelightInterface.java
+++ src/main/java/frc/robot/subsystems/LimelightInterface.java
@@ -1,33 +1,29 @@
 package main.java.frc.robot.subsystems;
 
-
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightInterface {
+    
+    // Get Limelight Instance 
     private static NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
 
-    public double getDoubleEntry(String entry) {
-        return limelight.getEntry(entry).getDouble(0);
-    }
-    public double[] getArrayEntry(String entry){
-        return limelight.getEntry(entry).getDoubleArray(new double[6]);
-    }
-    public double getID(){
-        return getDoubleEntry("tid");
-    }
-    public double getTargetArea(){
-        return getDoubleEntry("ta");
-    }
-    public boolean hasValidTargets(){
-        return getDoubleEntry("tv") == 1.0;
-    }
-    public double getXOffset(){
-        return getDoubleEntry("tx");
-    }
-    public double[] getBotPose(){
-        return getArrayEntry("botpose_targetspace");
-    }
+    @Override
+    public void periodic() {
+        NetworkTableEntry tx = limelight.getEntry("tx"); // Table X Value
+        NetworkTableEntry ty = limelight.getEntry("ty"); // Table Y Value
+        NetworkTableEntry ta = limelight.getEntry("ta"); // Table Area Value
 
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
 
+        //post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
 }
